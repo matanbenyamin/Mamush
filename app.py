5effcf176f9276e02ac93c86ee9c86944d7f4c05
Matan Benyamin <matan.benyamin@cu-bx.com>
2022-07-04 10:12:55 +0300

    imag table

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ import streamlit as st
 import os, sys
 
 @st.experimental_singleton
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
-
-_ = installff()
-from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
-
-browser.get('http://example.com')
-st.write(browser.page_source)
+# def installff():
+#   os.system('sbase install geckodriver')
+#   os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+#
+# _ = installff()
+# from selenium import webdriver
+# from selenium.webdriver import FirefoxOptions
+# opts = FirefoxOptions()
+# opts.add_argument("--headless")
+# browser = webdriver.Firefox(options=opts)
+#
+# browser.get('http://example.com')
+# st.write(browser.page_source)
 # streamlit run app.py
 
 TOKEN = "2034688341:AAGiWVOQZ_wJUDLEGPbqXn80LzJ9palMWu0"
